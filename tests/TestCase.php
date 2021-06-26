0b0933f2dced1c154cfad9e12a2512d4170976cf
frankyso <frankyso@users.noreply.github.com>
2021-06-26 07:24:43 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Konnco\Inadiv\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Konnco\Inadiv\InadivServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
