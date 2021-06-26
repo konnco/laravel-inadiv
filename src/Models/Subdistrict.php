a9a8e8058e88a79390444f558e0bface718ef043
frankyso <frankyso@users.noreply.github.com>
2021-06-26 09:49:19 +0000

    Fix styling

--- src/Models/Subdistrict.php
+++ src/Models/Subdistrict.php
@@ -4,7 +4,6 @@ namespace Konnco\Inadiv\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Subdistrict extends Model
 {
